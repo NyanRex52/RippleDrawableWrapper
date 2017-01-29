232d4c478bfa8f22d2502aef6705c1683e2b1966
Mohammed Al-Safwan <Nyanrex52@gmail.com>
2017-01-29 00:15:11 -0400

    Initial commit

---
+++ app/src/main/java/com/ripple/drawable/rippledrawablewrapper/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ripple.drawable.rippledrawablewrapper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/ripple/drawable/rippledrawablewrapper/RippleDrawableWrapper.java
@@ -0,0 +1,146 @@
+package com.ripple.drawable.rippledrawablewrapper;
+
+import android.content.res.ColorStateList;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.RippleDrawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
+import android.graphics.drawable.shapes.RectShape;
+import android.support.annotation.NonNull;
+import android.support.v7.graphics.Palette;
+
+/**
+ * Created by
+ *
+ * @author Mohammed Al-Safwan
+ * @date 2017-01-27
+ * for NyanRex Developments
+ * Contact at :nyanrex52@gmail.com
+ */
+
+public class RippleDrawableWrapper {
+
+    private float DEFAULT_ALPHA_FRACTION = 0.5f;
+    private float mAlphaFraction;
+
+    public RippleDrawableWrapper() {
+        mAlphaFraction = DEFAULT_ALPHA_FRACTION;
+    }
+
+    public RippleDrawableWrapper(float alphaFraction) {
+        mAlphaFraction = alphaFraction;
+    }
+
+    public Drawable getCircularDomColor(Bitmap admin) {
+        int color = Palette.from(admin).generate().getVibrantColor(Color.WHITE);
+        ColorStateList pressedColor = ColorStateList.valueOf(lightenOrDarken(color, 0.2D));
+        ColorDrawable defaultColor = new ColorDrawable(color);
+        Drawable rippleColor = getOvalRippleColor(color, admin.getWidth(), admin.getHeight());
+        return new RippleDrawable(
+                pressedColor,
+                null,
+                rippleColor
+        );
+    }
+
+    public Drawable getRecDomColor(Bitmap partyBanner) {
+        int color = Palette.from(partyBanner).generate().getVibrantColor(Color.WHITE);
+        ColorStateList pressedColor = ColorStateList.valueOf(lightenOrDarken(color, 0.2D));
+        ColorDrawable defaultColor = new ColorDrawable(color);
+        Drawable rippleColor = getRecRippleColor(color, partyBanner.getWidth(), partyBanner.getHeight());
+        return new RippleDrawable(
+                pressedColor,
+                null,
+                rippleColor
+        );
+    }
+
+    @NonNull
+    private Drawable getOvalRippleColor(int color, int width, int height) {
+//        float[] outerRadii = new float[8];
+//        Arrays.fill(outerRadii, 3);
+//        RoundRectShape r = new RoundRectShape(outerRadii, null, null);
+        ShapeDrawable shapeDrawable = new ShapeDrawable(new OvalShape());
+        shapeDrawable.setIntrinsicHeight(height);
+        shapeDrawable.setIntrinsicWidth(width);
+        shapeDrawable.getPaint().setColor(color);
+        return shapeDrawable;
+    }
+
+    @NonNull
+    private Drawable getRecRippleColor(int color, int width, int height) {
+//        float[] outerRadii = new float[8];
+//        Arrays.fill(outerRadii, 3);
+//        RoundRectShape r = new RoundRectShape(outerRadii, null, null);
+        ShapeDrawable shapeDrawable = new ShapeDrawable(new RectShape());
+        shapeDrawable.setIntrinsicHeight(height);
+        shapeDrawable.setIntrinsicWidth(width);
+        shapeDrawable.getPaint().setColor(color);
+        return shapeDrawable;
+    }
+
+    private int lightenOrDarken(int color, double fraction) {
+        if (canLighten(color, fraction)) {
+            return lighten(color, fraction);
+        } else {
+            return darken(color, fraction);
+        }
+    }
+
+    private int lighten(int color, double fraction) {
+        int red = Color.red(color);
+        int green = Color.green(color);
+        int blue = Color.blue(color);
+        red = lightenColor(red, fraction);
+        green = lightenColor(green, fraction);
+        blue = lightenColor(blue, fraction);
+        int alpha = getAlpha(color, mAlphaFraction);
+        return Color.argb(alpha, red, green, blue);
+    }
+
+    private int darken(int color, double fraction) {
+        int red = Color.red(color);
+        int green = Color.green(color);
+        int blue = Color.blue(color);
+        red = darkenColor(red, fraction);
+        green = darkenColor(green, fraction);
+        blue = darkenColor(blue, fraction);
+        int alpha = getAlpha(color, mAlphaFraction);
+
+        return Color.argb(alpha, red, green, blue);
+    }
+
+    private int getAlpha(int color, float alpha) {
+        return Math.round(Color.alpha(color) * alpha);
+    }
+
+    private boolean canLighten(int color, double fraction) {
+        int red = Color.red(color);
+        int green = Color.green(color);
+        int blue = Color.blue(color);
+        return canLightenComponent(red, fraction)
+                && canLightenComponent(green, fraction)
+                && canLightenComponent(blue, fraction);
+    }
+
+    private boolean canLightenComponent(int colorComponent, double fraction) {
+        int red = Color.red(colorComponent);
+        int green = Color.green(colorComponent);
+        int blue = Color.blue(colorComponent);
+        return red + (red * fraction) < 255
+                && green + (green * fraction) < 255
+                && blue + (blue * fraction) < 255;
+    }
+
+    private int darkenColor(int color, double fraction) {
+        return (int) Math.max(color - (color * fraction), 0);
+    }
+
+    private int lightenColor(int color, double fraction) {
+        return (int) Math.min(color + (color * fraction), 255);
+    }
+
+}
